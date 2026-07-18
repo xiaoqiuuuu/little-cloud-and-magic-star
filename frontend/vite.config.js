a5e7518424d72e52e1e6a78c7611a3d6a94a50fd
xiaoqiuuuu <weiqttt@qq.com>
2026-07-18 21:32:29 +0800

    chore: update local development defaults

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,11 +7,11 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'http://localhost:12222',
         changeOrigin: true,
       },
       '/uploads': {
-        target: 'http://localhost:8000',
+        target: 'http://localhost:12222',
         changeOrigin: true,
       }
     },
